f86db6628fe762484a871670f752fb367706a036
ShinoguKumatori <124132788+kumatorishinogu@users.noreply.github.com>
2023-02-25 14:07:36 +0000

    bookers2 途中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+   config.hosts << "d4bf80f81c104dfcbf40df854d57394b.vfs.cloud9.ap-northeast-1.amazonaws.com"
+   config.active_job.queue_adapter = :inline
 end
